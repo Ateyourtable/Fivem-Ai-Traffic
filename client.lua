49a643d7c188ebb8500cba1261de7bd4aa786a20
Ateyourtable <66190280+Ateyourtable@users.noreply.github.com>
2020-08-12 15:45:44 -0400

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,17 @@
+
+
+local config = {
+    pedFrequency = 1.0, -- 0.0 = No peds on streets, 1.0 normal ped ammounts on streets.
+    trafficFrequency = 1.0, -- 0.0 = No peds on streets, 1.0 normal ped ammounts on streets.
+}
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetPedDensityMultiplierThisFrame(config.pedFrequency)
+        SetScenarioPedDensityMultiplierThisFrame(config.pedFrequency, config.pedFrequency)
+        SetRandomVehicleDensityMultiplierThisFrame(config.trafficFrequency) 
+        SetParkedVehicleDensityMultiplierThisFrame(config.trafficFrequency) 
+        SetVehicleDensityMultiplierThisFrame(config.trafficFrequency)
+    end 
